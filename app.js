13c29ed2214b6b61047821918eb681e422865904
Vinay Kumar <149244101+codes4vinay@users.noreply.github.com>
2025-02-02 14:49:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 const app = express();
 
 const url = `https://app.filetranfer.tech/`;
-const interval = 900000;
+const interval = 880000;
 
 function reloadWebsite() {
   axios
